062e3c7d8567d769263ee59a615866ff92837344
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-14 14:46:35 -0300

    identificando os indices dos objetos

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let listaDeItens = [];
+let itemEditar
 
 const form = document.getElementById('form-itens');
 const itensInput = document.getElementById('receber-item');
@@ -53,11 +54,13 @@ function mostrarItem() {
         <li class="item-compra is-flex is-justify-content-space-between" 
           data-value="${index}">
           <div>
-          <input type="checkbox" class="is-clickable" />
-          <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
           </div>
           <div>
-          <i class="fa-solid fa-trash is-clickable deletar"></i>
+          
+    <i class="fa-regular fa-floppy-disk is-clickable"></i><i class="fa-regular is-clickable fa-pen-to-square editar"></i>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
           </div>
         </li>
       `
@@ -84,6 +87,17 @@ function mostrarItem() {
         .getAttribute('data-value');
       listaDeItens.splice(valorDoElemento, 1);
       mostrarItem();
+    })
+  })
+
+  const editarItens = document.querySelectorAll('.editar');
+
+  editarItens.forEach(i => {
+    i.addEventListener('click', (evento) => {
+      itemEditar = evento.target.parentElement.parentElement
+        .getAttribute('data-value');
+      mostrarItem();
+      console.log(itemEditar);
     })
   })
 }
