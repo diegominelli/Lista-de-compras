b63445d8e94b03a8a04ec62a4a11e283b681d19c
Diego Minelli <45859999+diegominelli@users.noreply.github.com>
2023-06-14 13:54:24 -0300

    fazendo a definicao de metodos

--- main.js
+++ main.js
@@ -2,15 +2,18 @@ let listaDeItens = [];
 
 const form = document.getElementById('form-itens');
 const itensInput = document.getElementById('receber-item');
+const ulItens = document.getElementById('lista-de-itens');
 
 form.addEventListener('submit', function(evento) {
   evento.preventDefault();
   salvarItem();
+  mostrarItem();
 });
 
 function salvarItem() {
   const comprasItem = itensInput.value;
-  const checarDuplicado = listaDeItens.some((elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase());
+  const checarDuplicado = listaDeItens.some((elemento) =>
+    elemento.valor.toUpperCase() === comprasItem.toUpperCase());
 
   if (checarDuplicado) {
     alert('Item já existe')
@@ -21,5 +24,24 @@ function salvarItem() {
   }
 
   console.log(listaDeItens);
+}
+
+function mostrarItem() {
+  ulItens.innerHTML = '';
+  listaDeItens.forEach((elemento, index) => {
+    ulItens.innerHTML +=
+      `
+        <li class="item-compra is-flex is-justify-content-space-between" 
+        data-value="${index}">
+          <div>
+              <input type="checkbox" class="is-clickable" />
+              <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+          </div>
+          <div>
+              <i class="fa-solid fa-trash is-clickable deletar"></i>
+          </div>
+        </li>
+      `
+  })
 }
 
